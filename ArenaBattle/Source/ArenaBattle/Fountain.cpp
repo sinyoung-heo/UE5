162fee15b57575543131ed0f7df223b8c3c7d88c
sinyoung-heo <hsykpu@gmail.com>
2024-12-01 17:41:06 +0900

    chapter03_UE_LOG 사용법
    액터 BeginPlay 시 간단한 로그 출력

--- ArenaBattle/Source/ArenaBattle/Fountain.cpp
+++ ArenaBattle/Source/ArenaBattle/Fountain.cpp
@@ -50,6 +50,7 @@ void AFountain::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	UE_LOG(ArenaBattle, Warning, TEXT("Actor Name : %s, ID : %d, Location X : %0.3f"), *GetName(), ID, GetActorLocation().X);
 }
 
 // Called every frame
