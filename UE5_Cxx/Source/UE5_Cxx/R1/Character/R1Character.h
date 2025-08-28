eded7dbed352e04c37bd09b83590389731fde542
sinyoung-heo <hsykpu@gmail.com>
2025-08-28 22:42:09 +0900

    22. Character
    Controller에 따른 캐릭터와 스프링암 Rotation 세팅 테스트

---
+++ UE5_Cxx/Source/UE5_Cxx/R1/Character/R1Character.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "R1Character.generated.h"
+
+UCLASS()
+class UE5_CXX_API AR1Character : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AR1Character();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<class USpringArmComponent> SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<class UCameraComponent> Camera;
+};
